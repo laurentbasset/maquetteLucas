d1833add1a82beadb28d48ffb2af5998861617aa
laurentbasset <l.basset@codeur.online>
2021-08-11 16:17:14 +0200

    reglage control element menu

--- js/main.js
+++ js/main.js
@@ -15,29 +15,29 @@ function toggleMenu(el) {
     }else{
         prt.classList.add('active');
         ctrl.style.visibility = "visible";
-        qte = ctrl.children[1].innerHTML = 1;
+        qte = ctrl.children[0].innerHTML = 1;
     }
     
 }
 
 function addToBasket(el) {
     var prt = el.parentNode;
-    var getQte = prt.children[1].innerHTML;
+    var getQte = prt.children[0].innerHTML;
     var qte = new Number(getQte);
     qte++;
-    prt.children[1].innerHTML = qte;    ;
+    prt.children[0].innerHTML = qte;    ;
     
 }
 
 function removeFromBasket(el) {
 
     var prt = el.parentNode;
-    var getQte = prt.children[1].innerHTML;
+    var getQte = prt.children[0].innerHTML;
     var qte = new Number(getQte);
     qte--;
     if(qte == 0) {
         var prtEl = el.parentNode; 
         toggleMenu(prtEl);
     }
-    prt.children[1].innerHTML = qte; 
+    prt.children[0].innerHTML = qte; 
 }
