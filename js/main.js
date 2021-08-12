22e13936d8bbccaa7b2450bdc8d831a778fb80f6
laurentbasset <l.basset@codeur.online>
2021-08-12 11:04:50 +0200

    reglage element de menu en flex

--- js/main.js
+++ js/main.js
@@ -4,43 +4,53 @@ function test($msg) {
 
 /* CONTROLES MENU */
 function toggleMenu(el) {
-    var prt = el.parentNode;
-    var ctrl = prt.children[2];
-    var qte = prt.children[1];
 
+    var prt = el.parentNode;
+    var ctrl = prt.children[1];
+    var qte = ctrl.children[0];
+    
     if(prt.className=="elmenu active") {
         prt.classList.remove('active');
-        ctrl.style.visibility = "hidden";
+        ctrl.style.display = "none";
         
-    }else{
-        prt.classList.add('active');
-        ctrl.style.visibility = "visible";
-        qte = ctrl.children[0].innerHTML = 1;
+     }else{
+         prt.classList.add('active');
+         ctrl.style.display = "inline-block";
+         qte = ctrl.children[0].innerHTML = 1;
     }
 }
 
 function addToBasket(el) {
     var prt = el.parentNode;
-    var getQte = prt.children[0].innerHTML;
+    var gprt = prt.parentNode;
+    var getQte = gprt.children[0].innerHTML;
     var qte = new Number(getQte);
     qte++;
-    prt.children[0].innerHTML = qte;    ;
-    
+    gprt.children[0].innerHTML = qte;    ;
 }
 
 function removeFromBasket(el) {
-
     var prt = el.parentNode;
     var gprt = prt.parentNode;
-    var getQte = prt.children[0].innerHTML;
+    var getQte = gprt.children[0].innerHTML;
     var qte = new Number(getQte);
     qte--;
     if(qte == 0) {
-        var prtEl = el.parentNode; 
-        toggleMenu(prtEl);
-        
-        //console.log (gprt.className == "elpanier active");
-        if(gprt.className == "elpanier active") { gprt.classList.remove('active'); }
+        toggleMenu(gprt);
     }
-    prt.children[0].innerHTML = qte; 
+    gprt.children[0].innerHTML = qte;    ;
+
+    // var prt = el.parentNode;
+    // var gprt = prt.parentNode;
+    // var getQte = prt.children[0].innerHTML;
+    // var qte = new Number(getQte);
+    // qte--;
+    // if(qte == 0) {
+    //     var prtEl = el.parentNode; 
+    //     toggleMenu(prtEl);
+        
+    //     //console.log (gprt.className == "elpanier active");
+    //     if(gprt.className == "elpanier active") { gprt.style.display ="none"; }
+    // }
+    // prt.children[0].innerHTML = qte; 
 }
